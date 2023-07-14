18e1984ecba72ff7b308959686435f3eb5bded15
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-07-15 00:05:20 +0200

    update

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,42 @@
-// const fs = require('fs/promises')
+import fs from 'fs/promises'
+import path from 'path'
 
-const listContacts = async () => {}
+const contactsPath = path.resolve('models','contacts.json');
 
-const getContactById = async (contactId) => {}
+ export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath)
+  const text = await data.toString()
+   return(text)
+}
 
-const removeContact = async (contactId) => {}
+export const getContactById = async (contactId) => {
+  const data = await fs.readFile(contactsPath)
+  const text = JSON.parse(data)
+  const sort = text.find(el => el.id === contactId)
+  if (sort) {
+   return(sort)
+  }else {
+   return(null)
+  }
+}
 
-const addContact = async (body) => {}
+export const removeContact = async (contactId) => {
+  const data = await fs.readFile(contactsPath)
+  const text = JSON.parse(data)
+  const deletCont = text.find(el => el.id === contactId)
+  const sort = text.filter(el => el.id !== contactId)
+const sortJS = JSON.stringify(sort)
+  const result = await fs.writeFile(contactsPath,sortJS)
+return(deletCont)
+}
+
+export const addContact = async (body) => {
+  const data = await fs.readFile(contactsPath)
+  const text = JSON.parse(data)
+  text.push(body)
+  const massJS = await JSON.stringify(text)
+  const result = await fs.writeFile(contactsPath,massJS)
+  return(body)
+}
 
 const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,31 @@
-const express = require('express')
-
+import express from 'express'
+import { listContacts } from '../../models/contacts.js'
+import { getContactById } from '../../models/contacts.js'
+import { removeContact } from '../../models/contacts.js'
+import { addContact } from '../../models/contacts.js'
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(listContacts())
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params
+  res.json(getContactById(id))
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params
+  res.json(addContact(id))
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params
+  res.json(removeContact(id))
 })
 
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-module.exports = router
+export default router
