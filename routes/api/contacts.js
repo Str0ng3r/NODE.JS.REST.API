d4b44db66a9d42c19ab58ec63ce9d8847f44439a
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-07-15 11:38:35 +0200

    PULL router

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,29 +3,29 @@ import { listContacts } from '../../models/contacts.js'
 import { getContactById } from '../../models/contacts.js'
 import { removeContact } from '../../models/contacts.js'
 import { addContact } from '../../models/contacts.js'
-const router = express.Router()
+const contactsRouter = express.Router()
 
-router.get('/', async (req, res, next) => {
+contactsRouter.get('/', async (req, res, next) => {
   res.json(listContacts())
 })
 
-router.get('/:contactId', async (req, res, next) => {
+contactsRouter.get('/:contactId', async (req, res, next) => {
   const id = req.params
   res.json(getContactById(id))
 })
 
-router.post('/', async (req, res, next) => {
+contactsRouter.post('/', async (req, res, next) => {
   const id = req.params
   res.json(addContact(id))
 })
 
-router.delete('/:contactId', async (req, res, next) => {
+contactsRouter.delete('/:contactId', async (req, res, next) => {
   const id = req.params
   res.json(removeContact(id))
 })
 
-router.put('/:contactId', async (req, res, next) => {
+contactsRouter.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-export default router
+export default contactsRouter
